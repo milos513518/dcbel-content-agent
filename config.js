d7e212b7ee626197fb8f002b069ddf97e8eae501
milos513518 <milos@ranchlab.is>
2025-08-25 15:37:23 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Configuration file for API keys and settings
+const CONFIG = {
+    OPENAI_API_KEY: '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',
+    APP_NAME: 'dcbel Content Agent',
+    API_MODEL: 'gpt-4'
+};
